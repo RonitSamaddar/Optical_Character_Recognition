d5bccfb5e38554636d43380f8a6caf8be8aac85a
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-07-09 11:55:05 +0530

    Trying tesseract on a few more plain text images with varying results

--- tesseract/tess.py
+++ tesseract/tess.py
@@ -16,14 +16,26 @@ cv2.imshow("Original Image",image)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Trying thresholding on the image
-if args["preprocess"] == "thresh":
-	gray = cv2.threshold(gray, 0, 255,
-		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+#if args["preprocess"] == "thresh":
+#gray = cv2.threshold(gray, 0, 255,
+#	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 """
 # Trying blur 
 elif args["preprocess"] == "blur":
-	gray = cv2.medianBlur(gray, 3)
-
 """
+#gray = cv2.medianBlur(gray, 3)
 
+
+
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, gray)
+
+
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print("Text in Image = "+text)
+# show the output images
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
